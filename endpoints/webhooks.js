9e11d582dadc9114a8f308ce47826c6398a09792
sindistrict <sindistrict.tv@gmail.com>
2019-11-01 10:43:01 -0700

    testing Plex webhooks

--- endpoints/webhooks.js
+++ endpoints/webhooks.js
@@ -2,10 +2,12 @@ const Axios = require('axios')
 
 module.exports = (server) => {
 
-  server.post('/api/webhooks-post', (req, res) => {
+  server.post('/api/webhooks', (req, res) => {
 
     Axios.post('https://discordapp.com/api/webhooks/639878507563843597/N-_JK6PsH-EHSV5YegdEvmqSgia5yWyrsL6ToevSonGPlWjMqaVQqclq7TmqskYISTmt', {
-      content: JSON.stringify(req.body)
+
+      content: JSON.stringify(req)
+      
     })
 
     res.status(200)
