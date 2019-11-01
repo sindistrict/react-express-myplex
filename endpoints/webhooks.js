cd75b037f07e427a3fc5663a31e720a45a5446b7
sindistrict <sindistrict.tv@gmail.com>
2019-11-01 11:29:42 -0700

    ...

--- endpoints/webhooks.js
+++ endpoints/webhooks.js
@@ -2,17 +2,16 @@ const Axios = require('axios')
 
 module.exports = (server) => {
 
-  server.post('/api/webhooks', (req, res) => {
+  server.post('/api/webhooks-plex', async (req, res, next) => {
+
+    const payload = JSON.parse(req.body.payload)
 
     Axios.post('https://discordapp.com/api/webhooks/639878507563843597/N-_JK6PsH-EHSV5YegdEvmqSgia5yWyrsL6ToevSonGPlWjMqaVQqclq7TmqskYISTmt', {
 
-      content: JSON.stringify(req.body)
+      content: payload
       
     })
 
-    res.status(200)
-    res.send()
-  
   })
 
 }
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require('path')
 
 const server = express()
 
+
 /**
  * Parse JSON HTTP requests.
  */
