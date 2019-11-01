3bb7c3a30c06a0e29b14ad8f8f62124a39a6e6f6
sindistrict <sindistrict.tv@gmail.com>
2019-11-01 10:38:24 -0700

    testing webhook endpoints

---
+++ endpoints/webhooks.js
@@ -0,0 +1,16 @@
+const Axios = require('axios')
+
+module.exports = (server) => {
+
+  server.post('/api/webhooks-post', (req, res) => {
+
+    Axios.post('https://discordapp.com/api/webhooks/639878507563843597/N-_JK6PsH-EHSV5YegdEvmqSgia5yWyrsL6ToevSonGPlWjMqaVQqclq7TmqskYISTmt', {
+      content: JSON.stringify(req.body)
+    })
+
+    res.status(200)
+    res.send()
+  
+  })
+
+}
--- index.js
+++ index.js
@@ -4,22 +4,45 @@ const path = require('path')
 
 const server = express()
 
+/**
+ * Parse JSON HTTP requests.
+ */
+
 server.use(parser.urlencoded({ extended: false }))
 server.use(parser.json())
 
+
+/**
+ * Use the React build folder as the static path for assets.
+ */
+
 server.use(express.static(path.join(__dirname, 'client/build')))
 
-require('./endpoints/GET/servers')(server)
-require('./endpoints/GET/libraries')(server)
-require('./endpoints/GET/users')(server)
 
-require('./endpoints/HOOKS/plex')(server)
+/**
+ * Listen to specific API endpoints.
+ */
+
+require('./endpoints/webhooks')(server)
+require('./endpoints/servers')(server)
+require('./endpoints/libraries')(server)
+require('./endpoints/users')(server)
+
+
+/**
+ * Send all other GET requests to the rendered index file.
+ */
 
 server.get('*', (req, res) => {
 
   res.sendFile(path.join(__dirname, 'client/build/index.html'))
 
 })
+
+
+/**
+ * Start the Express server and have it listen on port 5000.
+ */
 
 server.listen(process.env.PORT || 5000, () => {
 
